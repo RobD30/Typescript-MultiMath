b6c119a1bee02be66d3baa0a5568b4c08f214e88
Robert Dickerson <rdickeriii@gmail.com>
2017-10-23 10:54:33 -0700

    Refactor: setup of tsconfig and compiling
    end_of_line

--- app/app.js
+++ app/app.js
@@ -2,5 +2,6 @@ function startGame() {
     // Starting a new game
     var messagesElement = document.getElementById('messages');
     messagesElement.innerText = 'Welcome to MultiMath! Starting new game...';
+    console.log('Starting new game.');
 }
 document.getElementById('startGame').addEventListener('click', startGame);
--- js/app.js
+++ js/app.js
@@ -4,5 +4,5 @@ function startGame() {
     messagesElement.innerText = 'Welcome to MultiMath! Starting new game...';
     console.log('Starting new game.');
 }
-
 document.getElementById('startGame').addEventListener('click', startGame);
+//# sourceMappingURL=app.js.map
